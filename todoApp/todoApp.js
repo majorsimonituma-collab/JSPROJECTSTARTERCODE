327b71ee701c006b97205103bcd4d79b8bb110e2
Ituma Joseph <majorsimonituma@gmail.com>
2026-07-05 16:46:25 +0100

    tinkering on the todoApp project

---
+++ todoApp/todoApp.js
@@ -0,0 +1,22 @@
+'use strict'; 
+
+const addBtn = document.getElementById("add-button");
+const inputElement = document.getElementById("todo-input");
+const display = document.querySelector(".container")
+
+addBtn.addEventListener('click', function () {
+
+    const todoItem = inputElement.value
+
+    const heading = document.createElement("li");
+    const todolist = document.createElement("ul");
+    
+    const headingText = document.createTextNode(todoItem);
+
+    heading.appendChild(headingText);
+    todolist.appendChild(heading);
+    display.appendChild(todolist);
+
+
+    document.body.appendChild(display);
+})
